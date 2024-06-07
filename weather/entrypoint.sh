12a6a322262b0dbf2245d0eb921d984a5e94e052
hockton <you@example.com>
2024-06-07 15:15:06 +0300

    deleted comment lines

--- weather/entrypoint.sh
+++ weather/entrypoint.sh
@@ -2,11 +2,8 @@
 
 sleep 5
 
-# Django veritabanı migrasyonlarını çalıştır
 python manage.py migrate --noinput
 
-# Django statik dosyalarını klasör acıp toplar
 python manage.py collectstatic --noinput
 
-# Gunicorn ile uygulamayı başlat
 gunicorn --bind 0.0.0.0:8000 weather.wsgi:application
